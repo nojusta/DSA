74af56c26c209bf825d472f543df5b2d2819a2c8
Nojus Stankevičius <148903463+nojusta@users.noreply.github.com>
2024-05-14 14:49:17 +0300

    Create 2215.js

---
+++ leetCode/2215.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+
+    for(let num of nums1) {
+        if(set2.has(num)) {
+            set1.delete(num);
+            set2.delete(num);
+        }
+    }
+    return [[...set1], [...set2]];
+};
